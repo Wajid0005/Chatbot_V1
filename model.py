6e90ac6f2aa31855128e5d58739c5ef5825eaf62
Wajid Iqbal <005wajid@gmail.com>
2026-06-13 21:35:58 +0530

    Synchronize HF_TOKEN and HUGGINGFACEHUB_API_TOKEN for Streamlit Cloud

--- model.py
+++ model.py
@@ -8,13 +8,25 @@ load_dotenv()
 try:
     import streamlit as st
     if "HUGGINGFACEHUB_API_TOKEN" in st.secrets:
-        os.environ["HUGGINGFACEHUB_API_TOKEN"] = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+        token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+        os.environ["HUGGINGFACEHUB_API_TOKEN"] = token
+        os.environ["HF_TOKEN"] = token
+    if "HF_TOKEN" in st.secrets:
+        token = st.secrets["HF_TOKEN"]
+        os.environ["HUGGINGFACEHUB_API_TOKEN"] = token
+        os.environ["HF_TOKEN"] = token
 except Exception:
     pass
 
+# Synchronize local environment variables
+if os.environ.get("HUGGINGFACEHUB_API_TOKEN") and not os.environ.get("HF_TOKEN"):
+    os.environ["HF_TOKEN"] = os.environ["HUGGINGFACEHUB_API_TOKEN"]
+elif os.environ.get("HF_TOKEN") and not os.environ.get("HUGGINGFACEHUB_API_TOKEN"):
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.environ["HF_TOKEN"]
+
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 
-api_token = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
+api_token = os.environ.get("HF_TOKEN")
 
 llm = HuggingFaceEndpoint(
     repo_id="Qwen/Qwen2.5-7B-Instruct",
